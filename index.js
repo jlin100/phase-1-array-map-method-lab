fec76d1d4121cc30b1cba6a8db373ad8e6165caf
Jeff Lin <thejefflin@gmail.com>
2022-04-14 02:59:24 -0400

    commit tree ff469fd8c27091454549838560b4aaac39ff8f03

--- index.js
+++ index.js
@@ -12,5 +12,27 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let newArray = []
+  for (let i = 0; i < tutorials.length; i++) {
+    const newStr = tutorials[i].split(' ')
+   .map(w => w[0].toUpperCase() + w.substring(1))
+   .join(' ');
+newArray.push(newStr)
+  }
+  return newArray;
 }
+
+titleCased()
+
+// const titleCased = tutorials.map(tutorial);
+
+// function tutorial(string) {
+//   const str = string;
+//   const arr = str.split(" ");
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+//   }
+//   const str2 = arr.join(" ");
+//   console.log(str2);
+// }
+
